91a5ad89d7fa40751ad48a47e6bce7cae5b0873c
Shaun Mulligan <switchflip.shaun@gmail.com>
2023-04-19 13:49:45 +0200

    remove org.apache.http.legacy from termux

--- Termux/Android.mk
+++ Termux/Android.mk
@@ -4,7 +4,7 @@ LOCAL_MODULE := Termux
 LOCAL_SRC_FILES := com.quine.termux_arm64-v8a.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_OPTIONAL_USES_LIBRARIES := org.apache.http.legacy androidx.window.extensions androidx.window.sidecar
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_PRODUCT_MODULE := true
 include $(BUILD_PREBUILT)
