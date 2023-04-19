0c688895a2d2b2ccca44122eacc8e9993837b53b
Shaun Mulligan <switchflip.shaun@gmail.com>
2023-04-19 13:49:16 +0200

    remove org.apache.http.legacy from x11

--- TermuxX11/Android.mk
+++ TermuxX11/Android.mk
@@ -4,7 +4,7 @@ LOCAL_MODULE := TermuxX11
 LOCAL_SRC_FILES := termux-x11.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_OPTIONAL_USES_LIBRARIES := org.apache.http.legacy androidx.window.extensions androidx.window.sidecar
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_PRODUCT_MODULE := true
 include $(BUILD_PREBUILT)
