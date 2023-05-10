12244ecbc952c5f491dda83d5bb9d53caafaae05
Shaun Mulligan <switchflip.shaun@gmail.com>
2023-05-10 11:08:55 +0200

    add new quineLauncher

--- QuineLauncher/Android.mk
+++ QuineLauncher/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := QuineLauncher
-LOCAL_SRC_FILES := quinelauncher.apk
+LOCAL_SRC_FILES := quinelauncher-v0.0.1.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_OVERRIDES_PACKAGES := Trebuchet TrebuchetQuickStep TrebuchetGo TrebuchetQuickStepGo Home Launcher2 Launcher3 QuickSearchBox
